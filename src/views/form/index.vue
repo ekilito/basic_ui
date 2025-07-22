a9fe5d1e0918ac565f2e2231c09c5eb094be9535
kilito <wudalong@3h1china.com>
2025-07-22 17:08:46 +0800

    feat：抽取坐标校验

--- src/views/form/index.vue
+++ src/views/form/index.vue
@@ -17,6 +17,7 @@ import { ref, computed, h } from "vue";
 import { useAForm } from "../../hooks/useAForm.ts";
 import { type OptionItem } from "../../components/form/src/types/types.ts";
 import { ElMessage } from "element-plus";
+import { validatePosition } from "../../utils/index.ts";
 
 const formData = ref<any>({
   data: {
@@ -263,22 +264,7 @@ const formItems = computed<OptionItem[]>(() => [
         trigger: "change",
       },
       {
-        validator: (rule, value, callback) => {
-          const { longitude, latitude, altitude } = value || {};
-          if (longitude == null || latitude == null || altitude == null) {
-            return callback(new Error("经度、纬度和高度不能为空"));
-          }
-          if (longitude < -180 || longitude > 180) {
-            return callback(new Error("经度范围应在 -180 到 180 之间"));
-          }
-          if (latitude < -90 || latitude > 90) {
-            return callback(new Error("纬度范围应在 -90 到 90 之间"));
-          }
-          if (altitude < 0) {
-            return callback(new Error("高度不能为负数"));
-          }
-          callback();
-        },
+        validator: validatePosition,
         trigger: "change",
       },
     ],
