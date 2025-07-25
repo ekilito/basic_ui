eb331dc19ac93cbec4e297d8f9643d897cba1349
kilito <wudalong@3h1china.com>
2025-07-25 16:48:58 +0800

    feat: 定义table ts类型

--- src/components/table/src/index.vue
+++ src/components/table/src/index.vue
@@ -3,6 +3,19 @@
 </template>
 
 <script lang="ts" setup>
+import { type PropType } from 'vue';
+import type { TableOptions } from './types';
+
+const props = defineProps({
+  options: {
+   type: Array as PropType<TableOptions[]>,
+   required: true
+  },
+  data: {
+    type: Array as PropType<any[]>,
+     required: true
+  }
+})
 
 </script>
 
---
+++ src/components/table/src/types.ts
@@ -0,0 +1,25 @@
+export interface TableOptions {
+  label: string,
+  prop?: string,
+  width?: string | number,
+  align?: 'left' | 'center' | 'right',
+  type?: 'default' | 'selection' | 'index' | 'expand',
+  index?: string,
+  columnKey?: string,
+  minWidth?: string | number,
+  fixed?: 'left' | 'right' | boolean,
+  renderHeader?: (data: { column: any, $index: number }) => void,
+  sortable?: boolean | string,
+  sortMethod?: <T = any>(a: T, b: T) => number,
+  sortBy?: Function | string | Object,
+  sortOrders?: Object,
+  resizable?: boolean,
+  formatter?: Function,
+  showOverflowTooltip?: boolean | Object,
+  headerAlign?: 'left' | 'center' | 'right',
+  selectable?: Function,
+  reserveSelection?: boolean,
+  filters?: Array<{text: string, value: string}>,
+  
+  slot?: string
+}
