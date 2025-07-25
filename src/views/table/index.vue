1494d5ece1cb30993e7394a53dc79e707a430eef
kilito <wudalong@3h1china.com>
2025-07-25 16:59:14 +0800

    feat: 实现基础版表格

--- src/views/table/index.vue
+++ src/views/table/index.vue
@@ -1,7 +1,51 @@
 <template>
-  <a-table></a-table>
+  <a-table :data="tableData" :options="options"></a-table>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import type { TableOptions } from '../../components/table/src/types';
+
+
+const tableData = [
+  {
+    date: '2016-05-03',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles',
+  },
+  {
+    date: '2016-05-02',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles',
+  },
+  {
+    date: '2016-05-04',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles',
+  },
+  {
+    date: '2016-05-01',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles',
+  },
+]
+
+const options: TableOptions[] = [
+  {
+    label: 'Date',
+    prop: 'date',
+    align: 'center'
+  },
+  {
+    label: 'Name',
+    prop: 'name',
+    align: 'center'
+  },
+  {
+    label: 'Address',
+    prop: 'address',
+    align: 'center'
+  }
+]
+</script>
 
 <style lang="scss" scoped></style>
